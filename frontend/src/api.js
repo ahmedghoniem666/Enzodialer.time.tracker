4d5c0573f128bedee14526ca3e7f79f1cc30ac77
ahmedghoniem666 <ahmedibrahim66333@gmail.com>
2025-08-21 20:54:06 +0300

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-export const API = (process.env.VITE_API_URL) || 'http://localhost:4000';
-export const post = (path, body, token) => axios.post(API + path, body, { headers: token ? { Authorization: 'Bearer ' + token } : {} }).then(r => r.data);
-export const get = (path, token) => axios.get(API + path, { headers: token ? { Authorization: 'Bearer ' + token } : {} }).then(r => r.data);
